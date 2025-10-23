90b503b78a52a6cf4a2dde6bb20ccff78764673b
Rary Coringa <rary.goncalves.123@ufrn.edu.br>
2025-10-23 09:15:04 -0300

    remove EnableCaching annotation

--- src/main/java/br/edu/ufrn/feed/FeedApplication.java
+++ src/main/java/br/edu/ufrn/feed/FeedApplication.java
@@ -2,11 +2,9 @@ package br.edu.ufrn.feed;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.web.reactive.config.EnableWebFlux;
 
 @EnableWebFlux
-@EnableCaching
 @SpringBootApplication
 public class FeedApplication {
 
